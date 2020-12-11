49338fb2528f45d98f69d5fcfdce5896d708a867
Leonardo <oliveirabalsa2@gmail.com>
2020-12-11 14:06:05 -0300

    feat: create UserCard component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-card class="user__card">
+    <q-card-section>
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">
+          {{ repositoryName }}
+        </div>
+        <div
+          class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+        >
+          <q-icon name="person" />
+          {{ owner }}
+        </div>
+      </div>
+
+      <q-rating v-model="stars" size="26px" />
+      {{ stars }}
+    </q-card-section>
+    <q-separator />
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  data() {
+    return {};
+  },
+  props: {
+    repositoryName: {
+      type: String,
+      default: '',
+      required: true,
+    },
+
+    stars: {
+      type: String,
+      default: '',
+      required: true,
+    },
+
+    owner: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.user__card {
+  width: 60%;
+  margin-top: 5px;
+}
+</style>
